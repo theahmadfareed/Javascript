f8d8fbcd96c09162fa18a72c0db519b3893845d8
theahmadfareed <ha5403905@gmail.com>
2024-02-12 16:54:18 +0500

    4 small projects added.

---
+++ Concepts/11-DOM/script.js
@@ -0,0 +1,68 @@
+console.log("DOM Manuplation!");
+
+// two
+const parent = document.querySelector(".parent");
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[1].innerHTML);
+
+// for (let i = 0; i < parent.children.length; i++) {
+//      console.log(parent.children[i].innerHTML);
+
+// }
+parent.children[1].style.color = "orange";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+const dayOne = document.querySelector(".day");
+// console.log(dayOne);
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log("NODES: ", parent.childNodes);
+
+// three
+const div = document.createElement("div");
+console.log(div);
+div.className = "main";
+div.id = Math.round(Math.random() * 10 + 1);
+div.setAttribute("title", "generated title");
+div.style.backgroundColor = "green";
+div.style.padding = "12px";
+// div.innerText = "Chai aur code"
+const addText = document.createTextNode("Chai aur code");
+div.appendChild(addText);
+
+document.body.appendChild(div);
+
+// 4-CRUD on DOM
+function addLanguage(langName) {
+  const li = document.createElement("li");
+  li.innerHTML = `${langName}`;
+  document.querySelector(".language").appendChild(li);
+}
+addLanguage("python");
+addLanguage("typescript");
+
+function addOptiLanguage(langName) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(langName));
+  document.querySelector(".language").appendChild(li);
+}
+addOptiLanguage("golang");
+
+//Edit
+const secondLang = document.querySelector("li:nth-child(2)");
+console.log(secondLang);
+//secondLang.innerHTML = "Mojo"
+const newli = document.createElement("li");
+newli.textContent = "Mojo";
+secondLang.replaceWith(newli);
+
+//edit
+const firstLang = document.querySelector("li:first-child");
+firstLang.outerHTML = "<li>TypeScript</li>";
+
+//remove
+const lastLang = document.querySelector("li:last-child");
+lastLang.remove();
