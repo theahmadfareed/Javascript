e973bf4e770b89aff714386493434262b5dc776c
theahmadfareed <ha5403905@gmail.com>
2024-02-16 18:20:43 +0500

    tailwind setup, components and props.

---
+++ Web Development/Frontend/React/Concepts/Code aur React/04-Components-Props-Tailwind/03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />
+    </>
+  )
+}
+
+export default App
