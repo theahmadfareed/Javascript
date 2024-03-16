7bba948fae6a9d008b75bfa90e48523de16ceb07
theahmadfareed <ha5403905@gmail.com>
2024-03-17 01:18:12 +0500

    farm fyp project added in python web development folder.

--- Web Development/Stacks/FARM/FYP/frontend/src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.png";
-import keywords from "./keywords.jpg";
-import avatar from "./avatar.png";
-import reddit from "./reddit.png";
-
-export { logo };
-export { keywords };
-export { avatar };
-export { reddit };
